4768c8b1bcf8e1b73786874bbf1213267a7cef44
Jeppss <95717767+Jeppss@users.noreply.github.com>
2022-05-07 00:04:06 +0200

    Add files via upload

---
+++ corr.py
@@ -0,0 +1,26 @@
+import pandas_datareader as pdr
+import numpy as np
+
+filecoin = pdr.DataReader(("NBX.OL"),
+                       start='2022-1-1', 
+                       end='2022-5-1', 
+                       data_source='yahoo')['Close']
+x = list()
+for i in filecoin:
+    x.append(i)
+x = np.array(x)
+
+dltx = pdr.DataReader(("DLTX.OL"),
+                    start='2022-1-1',
+                    end='2022-5-1',
+                    data_source='yahoo')['Close']
+
+y = list()
+for i in dltx:
+    y.append(i)
+
+y = np.array(y)
+
+
+r = np.corrcoef(x, y)
+print(r)
